301bc89dafa19c401b5238a3549c355d660e87bb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-08-16 02:19:26 +0530

    changes by me

--- 04/iter.py
+++ 04/iter.py
@@ -1,3 +1,11 @@
 numbers = [1,2,3,4,5]
+phones = {'sudhakar': 1, 'krupakar':2, 'latha': 3}
 it = iter(numbers)
 print (it.__next__())
+print(next(it))
+
+rfile = open('grades.txt','r')
+print (next(rfile),end='')
+
+it = iter(phones)
+print (next(it))
